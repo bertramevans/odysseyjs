ea839307753aa45f43e1ce14d1e26783eee505d3
bertramevans <115526089+bertramevans@users.noreply.github.com>
2022-10-11 21:15:54 +0700

    revert to apigithubgists

--- sandbox/gist.js
+++ sandbox/gist.js
@@ -85,8 +85,8 @@ function Gist(md, template, callback) {
       },
     };
 
-    // d3.xhr("https://raw.githubusercontent.com/bertramevans/test4/main/api_github_com_gist.json")
-    d3.json("api_github_com_gist.json")
+    d3.xhr("https://api.github.com/gists")
+    // d3.json("api_github_com_gist.json")
       .header("Content-Type", "application/json")
       .post(JSON.stringify(payload), function (err, xhr) {
         gist = JSON.parse(xhr.responseText);
